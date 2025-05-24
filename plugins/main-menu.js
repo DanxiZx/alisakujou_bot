743c792df4a435f1b047e5b9e6d90a10715ef852
DanxiZx <diegolesimarcelino@gmail.com>
2025-05-24 13:01:57 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,19 +8,22 @@ let handler = async (m, { conn, args }) => {
     let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
     let txt = `
-Hola! Soy  *${botname}*  ٩(˘◡˘)۶
-Aquí tienes la lista de comandos
-╭┈ ↷
-│ᰔᩚ Cliente » @${userId.split('@')[0]}
-│❀ Modo » Publico
-│✦ Bot » ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Prem Bot 🅑')}
-│ⴵ Activada » ${uptime}
-│✰ Usuarios » ${totalreg}
-│✧ Comandos » ${totalCommands}
-│🜸 Baileys » Multi Device
-╰─────────────────
-Crea un *Sub-Bot* con tu número utilizando *#qr* o *#code*
+Hola! Soy  *${botname}*  🤍
+*Aquí tienes la lista de comandos*
 
+╭┈─────────────────────
+│🙎🏻‍♂️ Cliente » @${userId.split('@')[0]}
+│🌍 Modo » Publico
+│🌸 Bot » ${(conn.user.jid == global.conn.user.jid ? 'Principal 💻' : 'Prem Bot 💎')}
+│⏳ Activada » ${uptime}
+│🪪 Usuarios » ${totalreg}
+│📃 Comandos » ${totalCommands}
+│🔰 Baileys » Multi Device
+╰──────────────────────────────────────
+Crea un *Sub-Bot* con tu número utilizando *#qr* o *#code*
+─────────────────────────────────────────────────
+𝘊𝘢𝘯𝘢𝘭 𝘖𝘧𝘪𝘤𝘪𝘢𝘭 𝘋𝘦 𝘓𝘢 𝘉𝘰𝘵 https://whatsapp.com/channel/0029VbAoNZxDZ4Lk1WmUyZ3I
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 • :･ﾟ⊹˚• \`『 Info-Bot 』\` •˚⊹:･ﾟ•
 
 ❍ Comandos para ver estado e información de la Bot.
